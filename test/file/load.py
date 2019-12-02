fbb72cb801a7f83a4164b0414b001b580a6ad02d
TFGK <deee5@naver.com>
2019-12-02 23:57:59 +0900

    [음..[

--- test/file/load.py
+++
@@ -1,7 +0,0 @@
-import pickle
-
-with open('config.pkl', 'rb') as file:
-    data = pickle.load(file)
-
-print(data)
-
--- test/file/readMe.txt
+++
@@ -1,5 +0,0 @@
-use pip install:
-
-pip install numpy
-pip install opencv-python
-pip install opencv-contrib-python
